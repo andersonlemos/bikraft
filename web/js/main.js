438dfb245fb7a911acce308892edcaf0fea3b053
andersonlemos <anderson.lemos@dmorj.com.br>
2016-12-12 17:24:44 -0200

    update plugins(slides)

--- web/js/main.js
+++ web/js/main.js
@@ -1,5 +1,11 @@
 $(".rslides").responsiveSlides({
   auto: true,             // Boolean: Animate automatically, true or false
   speed: 500,            // Integer: Speed of the transition, in milliseconds
-  timeout: 3000,          // Integer: Time between slide transitions, in milliseconds
+  timeout: 5000          // Integer: Time between slide transitions, in milliseconds
+});
+$(".rslides_portfolio").responsiveSlides({
+  auto: true,             // Boolean: Animate automatically, true or false
+  speed: 500,            // Integer: Speed of the transition, in milliseconds
+  timeout: 5000,          // Integer: Time between slide transitions, in milliseconds
+  pager: true
 });
