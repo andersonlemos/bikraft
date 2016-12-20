3d7ddd62056a10d54a8d513c4bcdc72ed8b9c8f7
andersonlemos <anderson.lemos@dmorj.com.br>
2016-12-20 17:23:44 -0200

    final

--- web/js/main.js
+++ web/js/main.js
@@ -39,27 +39,27 @@ Visibility.onVisible(function(){
 // Formulario
 
 $('.formphp').on('submit', function() {
-	var emailContato = "bdl182@gmail.com"; // Escreva aqui o seu e-mail
+	var emailContato = "contato@bikcraft.com"; // Escreva aqui o seu e-mail
 
 	var that = $(this),
 			url = that.attr('action'),
 			type = that.attr('method'),
 			data = {};
-
+	
 	that.find('[name]').each(function(index, value) {
 		var that = $(this),
 				name = that.attr('name'),
 				value = that.val();
-
+				
 		data[name] = value;
 	});
-
+	
 	$.ajax({
 		url: url,
 		type: type,
 		data: data,
 		success: function(response) {
-
+		
 			if( $('[name="leaveblank"]').val().length != 0 ) {
 				$('.formphp').html("<div id='form-erro'></div>");
 				$('#form-erro').html("<span>Falha no envio!</span><p>Você pode tentar novamente, ou enviar direto para o e-mail " + emailContato + " </p>")
@@ -68,7 +68,7 @@ $('.formphp').on('submit', function() {
 				$('#form-erro');
 				});
 			} else {
-
+			
 				$('.formphp').html("<div id='form-send'></div>");
 				$('#form-send').html("<span>Mensagem enviada!</span><p>Em breve eu entro em contato com você. Abraços.</p>")
 				.hide()
@@ -82,10 +82,10 @@ $('.formphp').on('submit', function() {
 			$('#form-erro').html("<span>Falha no envio!</span><p>Você pode tentar novamente, ou enviar direto para o e-mail " + emailContato + " </p>")
 			.hide()
 			.fadeIn(1500, function() {
-			$('#form-erro');
+			$('#form-erro');  
 		});
 		}
 	});
-
+	
 	return false;
 });
